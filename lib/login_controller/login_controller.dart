98a3cf83ce9a00aa762f4ca19bc2865a2a02193d
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-28 21:19:15 +0600

    GetX - Post (Api) #2

--- lib/login_controller/login_controller.dart
+++ lib/login_controller/login_controller.dart
@@ -1,31 +1,33 @@
-
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart';
 
 class LoginController extends GetxController {
-
   final emailController = TextEditingController().obs;
   final passwordController = TextEditingController().obs;
 
-  void loginApi()async{
+  void loginApi() async {
+    try {
+      final response =
+          await post(Uri.parse('https://reqres.in/api/login'),
+          body: {
+        'email': 'emailController.value.text',
+        'password': 'passwordController.value.text',
+      });
 
-    final response = await post(Uri.parse('https://reqres.in/api/login'),
-        body: {
-          'email' : '',
-          'password' : '',
-        });
+      var data = jsonDecode(response.body);
+      print(response.statusCode);
+      print(data);
 
-    var data = jsonDecode(response.body);
-    print(response.statusCode);
-    print(data);
+      if (response.statusCode == 200) {
+        Get.snackbar('Login Successfully', data['error']);
 
-    if(response.statusCode == 200){
-
-    }else {
-
+      } else {
+        Get.snackbar('Login Field', data['error']);
+      }
+    } catch (e) {
+      Get.snackbar('Exception', e.toString());
     }
   }
 }
